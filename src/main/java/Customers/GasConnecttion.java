efd17b82c40ef9da5d544972d4fe8e740b89140f
Rohit Das <powerrangers.sami@gmail.com>
2025-03-14 09:06:45 +0530

    added GasConnection class

--- src/main/java/Customers/GasConnecttion.java
+++ src/main/java/Customers/GasConnecttion.java
@@ -1,6 +1,27 @@
 package Customers;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
 public class GasConnecttion extends Customer {
-
-
+    int numberOfCylinders;
+    String date;
+    static int connectionNumber = 100;
+    static {
+        connectionNumber += 1;
+    }
+    Date lastDate = null;
+    SimpleDateFormat dateFormat = new SimpleDateFormat( "dd-MM-yyyy" );
+    public GasConnecttion(String name, String street, String area, String pincode, String mobile) {
+        super(name, street, area, pincode, mobile);
+        this.numberOfCylinders = numberOfCylinders;
+    }
+    public void getLastDate() {
+        System.out.println("Enter the last Date");
+        date = new Scanner(System.in).nextLine();
+        try {
+            lastDate = dateFormat.parse(date);
+        } catch (Exception e) {
+            System.out.println("error in getLast Date: " + e.getMessage());
+        }
+    }
 }
